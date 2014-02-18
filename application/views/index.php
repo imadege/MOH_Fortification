5a3b6a1852205e713fc34cfacab3bed7a08aca8a
Ian Madege <imadege1990@gmail.com>
2014-02-18 13:20:45 -0800

    fixes on template change

--- application/views/index.php
+++ application/views/index.php
@@ -1,54 +1,231 @@
-<html>
-	<head>
-		<link href="<?php echo base_url(); ?>css/layout.css" rel="stylesheet" type="text/css" />
-		<!-- -->
-		<!-- Attach CSS files -->
-		<link rel="stylesheet" href="<?php echo base_url(); ?>css/styles.css"/>
-	
-		<script src="js/jquery-1.7.2.min.js" type="text/javascript"></script>
-		<script src="js/jquery.orbit.js" type="text/javascript"></script>
-	</head>
-	<body>
-		<section class="banner">
-			<section class="logo">
-				<img src="<?php echo base_url(); ?>images/coat_of_arms-resized.png" />
 
-			</section>
-			<section class="credentials">
-				<section class="title">
-					Ministry Of Public Health and Sanitation
-				</section>
-				<section class="subtitle">
-					Department of Family Health<p></p>
-				</section>
-				<section class="division">
-					Division of Nutrition<p></p><p></p>
-				</section>
-			</section>
-			<section class="date">
+<!DOCTYPE html>
+<html lang="en">
 
-				<?php echo date("l F d, Y"); ?>
-			</section>
-		</section>
+<?php $this->load->view("asset") ?>
+<body class="login-layout">
+<div class="main-container container-fluid">
+<div class="main-content">
+<div class="row-fluid">
+<div class="span12">
+<div class="login-container">
+<div class="row-fluid">
+    <div class="center">
+        <h1>
+            <i class="icon-leaf green"></i>
+            <span class="red">Ace</span>
+            <span class="white">Application</span>
+        </h1>
+        <h4 class="blue">&copy; Company Name</h4>
+    </div>
+</div>
 
-		<section class="message">
-			<?php echo $form; ?>
-		</section>
-		<section class="login">
-			<section class="form-title">
-				<section class="title-text">
-					Sign In
-				</section>
-			</section>
-			<form class="form-login" method="post" accept-charset="utf-8">
+<div class="space-6"></div>
 
-				<input name="username" type="text" placeholder="Username / Email Address"/>
-				<p></p>
-				<input  name="secret" type="password" placeholder="Password"/>
-				<p></p>
-				<button type="submit" class="awesome myblue large" formaction="<?php echo base_url().'c_auth/login'?>" />
-				Login</button>
-			</form>
-		</section>
-	</body>
-</html>
+<div class="row-fluid">
+    <div class="position-relative">
+        <div id="login-box" class="login-box visible widget-box no-border">
+            <div class="widget-body">
+                <div class="widget-main">
+                    <h4 class="header blue lighter bigger">
+                        <i class="icon-coffee green"></i>
+                        Please Enter Your Information
+                    </h4>
+
+                    <div class="space-6"></div>
+
+                    <form>
+                        <fieldset>
+                            <label>
+															<span class="block input-icon input-icon-right">
+																<input type="text" class="span12" placeholder="Username" />
+																<i class="icon-user"></i>
+															</span>
+                            </label>
+
+                            <label>
+															<span class="block input-icon input-icon-right">
+																<input type="password" class="span12" placeholder="Password" />
+																<i class="icon-lock"></i>
+															</span>
+                            </label>
+
+                            <div class="space"></div>
+
+                            <div class="clearfix">
+                                <label class="inline">
+                                    <input type="checkbox" />
+                                    <span class="lbl"> Remember Me</span>
+                                </label>
+
+                                <button onclick="return false;" class="width-35 pull-right btn btn-small btn-primary">
+                                    <i class="icon-key"></i>
+                                    Login
+                                </button>
+                            </div>
+
+                            <div class="space-4"></div>
+                        </fieldset>
+                    </form>
+
+                    <div class="social-or-login center">
+                        <span class="bigger-110">Or Login Using</span>
+                    </div>
+
+                    <div class="social-login center">
+                        <a class="btn btn-primary">
+                            <i class="icon-facebook"></i>
+                        </a>
+
+                        <a class="btn btn-info">
+                            <i class="icon-twitter"></i>
+                        </a>
+
+                        <a class="btn btn-danger">
+                            <i class="icon-google-plus"></i>
+                        </a>
+                    </div>
+                </div><!--/widget-main-->
+
+                <div class="toolbar clearfix">
+                    <div>
+                        <a href="login.html#" onclick="show_box('forgot-box'); return false;" class="forgot-password-link">
+                            <i class="icon-arrow-left"></i>
+                            I forgot my password
+                        </a>
+                    </div>
+
+                    <div>
+                        <a href="login.html#" onclick="show_box('signup-box'); return false;" class="user-signup-link">
+                            I want to register
+                            <i class="icon-arrow-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div><!--/widget-body-->
+        </div><!--/login-box-->
+
+        <div id="forgot-box" class="forgot-box widget-box no-border">
+            <div class="widget-body">
+                <div class="widget-main">
+                    <h4 class="header red lighter bigger">
+                        <i class="icon-key"></i>
+                        Retrieve Password
+                    </h4>
+
+                    <div class="space-6"></div>
+                    <p>
+                        Enter your email and to receive instructions
+                    </p>
+
+                    <form>
+                        <fieldset>
+                            <label>
+															<span class="block input-icon input-icon-right">
+																<input type="email" class="span12" placeholder="Email" />
+																<i class="icon-envelope"></i>
+															</span>
+                            </label>
+
+                            <div class="clearfix">
+                                <button onclick="return false;" class="width-35 pull-right btn btn-small btn-danger">
+                                    <i class="icon-lightbulb"></i>
+                                    Send Me!
+                                </button>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div><!--/widget-main-->
+
+                <div class="toolbar center">
+                    <a href="login.html#" onclick="show_box('login-box'); return false;" class="back-to-login-link">
+                        Back to login
+                        <i class="icon-arrow-right"></i>
+                    </a>
+                </div>
+            </div><!--/widget-body-->
+        </div><!--/forgot-box-->
+
+        <div id="signup-box" class="signup-box widget-box no-border">
+            <div class="widget-body">
+                <div class="widget-main">
+                    <h4 class="header green lighter bigger">
+                        <i class="icon-group blue"></i>
+                        New User Registration
+                    </h4>
+
+                    <div class="space-6"></div>
+                    <p> Enter your details to begin: </p>
+
+                    <form>
+                        <fieldset>
+                            <label>
+															<span class="block input-icon input-icon-right">
+																<input type="email" class="span12" placeholder="Email" />
+																<i class="icon-envelope"></i>
+															</span>
+                            </label>
+
+                            <label>
+															<span class="block input-icon input-icon-right">
+																<input type="text" class="span12" placeholder="Username" />
+																<i class="icon-user"></i>
+															</span>
+                            </label>
+
+                            <label>
+															<span class="block input-icon input-icon-right">
+																<input type="password" class="span12" placeholder="Password" />
+																<i class="icon-lock"></i>
+															</span>
+                            </label>
+
+                            <label>
+															<span class="block input-icon input-icon-right">
+																<input type="password" class="span12" placeholder="Repeat password" />
+																<i class="icon-retweet"></i>
+															</span>
+                            </label>
+
+                            <label>
+                                <input type="checkbox" />
+															<span class="lbl">
+																I accept the
+																<a href="login.html#">User Agreement</a>
+															</span>
+                            </label>
+
+                            <div class="space-24"></div>
+
+                            <div class="clearfix">
+                                <button type="reset" class="width-30 pull-left btn btn-small">
+                                    <i class="icon-refresh"></i>
+                                    Reset
+                                </button>
+
+                                <button onclick="return false;" class="width-65 pull-right btn btn-small btn-success">
+                                    Register
+                                    <i class="icon-arrow-right icon-on-right"></i>
+                                </button>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+
+                <div class="toolbar center">
+                    <a href="login.html#" onclick="show_box('login-box'); return false;" class="back-to-login-link">
+                        <i class="icon-arrow-left"></i>
+                        Back to login
+                    </a>
+                </div>
+            </div><!--/widget-body-->
+        </div><!--/signup-box-->
+    </div><!--/position-relative-->
+</div>
+</div>
+</div><!--/.span-->
+</div><!--/.row-fluid-->
+</div>
+</div><!--/.main-container-->
+
+<?php $this->load->view("footer") ?>
